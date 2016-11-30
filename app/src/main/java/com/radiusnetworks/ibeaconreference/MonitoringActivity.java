7a378eb2496fa3644f61bc85b63932ed6ee86035
永日~ <www10240mb@gmail.com>
2016-12-01 07:53:17 +0800

    Update MonitoringActivity.java

--- app/src/main/java/com/radiusnetworks/ibeaconreference/MonitoringActivity.java
+++ app/src/main/java/com/radiusnetworks/ibeaconreference/MonitoringActivity.java
@@ -53,6 +53,14 @@ public class MonitoringActivity extends Activity implements IBeaconConsumer {
 		dbSQLite = new SQLite(this, MonitoringActivity.this);
 		cNTP = new NTPClient();
 		showView = true;
+		
+						Thread thread = new Thread(){
+							public void run(){
+								String _TIME = cNTP.getTime(4);
+								dbSQLite.add(dbSQLite.tableName_Beacon, _UUID, _INFO, _INFO2, _TIME);
+							}
+						};
+						thread.start();
 	}
 
 	@Override
@@ -185,6 +193,7 @@ public class MonitoringActivity extends Activity implements IBeaconConsumer {
 			return arg0;
 		}
 
+		@TargetApi(Build.VERSION_CODES.N)
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			try {
@@ -211,7 +220,12 @@ public class MonitoringActivity extends Activity implements IBeaconConsumer {
 
 						Thread thread = new Thread(){
 							public void run(){
-								String _TIME = cNTP.getTime(4);
+								//String _TIME = cNTP.getTime(4);
+								
+								Calendar calendar = Calendar.getInstance();
+
+								String _TIME = calendar.get(Calendar.YEAR) + "/" + calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DAY_OF_MONTH) + " - " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND) + ":" + calendar.get(Calendar.MILLISECOND);
+								Log.d("TIME", _TIME);
 								dbSQLite.add(dbSQLite.tableName_Beacon, _UUID, _INFO, _INFO2, _TIME);
 							}
 						};
